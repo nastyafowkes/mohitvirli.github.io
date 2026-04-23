907a2e0a00ac96563a6aaf5d5112577bcaabe4bd
nastyafowkes <nastya.karasevych1@gmail.com>
2026-04-23 13:40:35 +0300

    Update projects.ts

--- app/constants/projects.ts
+++ app/constants/projects.ts
@@ -3,31 +3,31 @@ import { Project } from "../types";
 // TODO: Move this to API
 export const PROJECTS: Project[] = [
   {
-    title: 'Pressure Washing Business Site',
+    title: 'Service Site',
     date: 'Dec 2025',
     subtext: 'A basic starter project to initialize project using HTML. It  uses standard app structure with animation, service grids, pricing tiers  and contact forms.',
     url: 'https://hydroforce0.netlify.app/',
   },
   {
-    title: 'Charitable Landing Page Design',
+    title: 'Charitable Page',
     date: 'Jan 2026',
     subtext: 'A lightweight library for turning objects into observables, providing a convenient API for accessing archives and charitable foundraising page with a bot and managable links, mobile-friendly.',
     url: 'https://anastasia-home.netlify.app/',
   },
   {
-    title: 'Local Service Company Websites',
+    title: 'Service Page',
     date: 'March 2026',
     subtext: 'An HTML tool to manage buisness for development. It enables quick and easy English or Ukrainian mapping to URLs with a single command to a Bot that sends alerts for the buisness about ordering services and contact info.',
     url: 'https://webcraft0.netlify.app/',
   },
   {
-    title: 'BYU Client-Style Briefs',
+    title: 'BYU Briefs',
     date: 'April 2026',
     subtext: 'A landing page with animation, GIFs that help to study materials with interactive sorting tasks.',
     url: 'https://perseverancebyu.netlify.app/',
   },
   {
-    title: 'Landing Page for English School',
+    title: 'English School',
     date: 'Apr 2026',
     subtext: 'Developed in Ukrainian a minimalist landing page with animation and future photo usage(when will be provided by the client) and price adjusting, creating a whatsup bot  with linked  bank card.',
     url: 'https://smart-class-studyenglish.netlify.app/',
